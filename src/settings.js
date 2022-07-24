0daf3c990b3a10efadfb9920ed3b6ba61533b465
senspond20 <senspond@gmail.com>
2022-07-24 23:53:01 +0900

    first commit

---
+++ src/settings.js
@@ -0,0 +1,31 @@
+module.exports = {
+    title: 'VUE Project',
+    /**
+     * @type {boolean} true | false
+     * @description 오른쪽 패널을 보여줄지 선택한다.
+     */
+    showSettings: true,
+    /**
+     * @type {boolean} true | false
+     * @description 탭뷰를 보여줄지 설정
+     */
+    tagsView: true,
+
+    /**
+     * @type {boolean} true | false
+     * @description 해더를 고정할지 설정
+     */
+    fixedHeader: true,
+
+    /**
+     * @type {boolean} true | false
+     * @description 로고를 표시할지 설정
+     */
+    sidebarLogo: true,
+
+    /**
+     * @type {string | array} 'production' | ['production', 'development']
+     * @description 에러로그를 보여줄 필요가 있는지
+     */
+    errorLog: 'production'
+}
